af389172e80b6134bd646ec1101805b5020e72f3
francescocaredda <francecaredda6@gmail.com>
2024-09-26 17:22:37 +0200

    removed the graphical dependencies

--- src/IsingSimulator.jl
+++ src/IsingSimulator.jl
@@ -2,7 +2,7 @@ __precompile__(false) # precompilation stopped working in Julia 1.10
 module IsingSimulator
 using DataFrames
 import Base: BufferStream
-using Random, Statistics,GLMakie, QuadGK
+using Random, Statistics #,GLMakie, QuadGK
 import ProgressMeter: Progress, next!
 import ForwardDiff: derivative
 export isingview,mytest,βc2d,magn2d,free_energy2d,observables
@@ -10,7 +10,7 @@ export isingview,mytest,βc2d,magn2d,free_energy2d,observables
 const βc2d = log(1 + √2) / 2 # critical temperature 2d ising FM model
 include("lattice.jl")
 include("mc.jl")
-include("isingview.jl")
+#include("isingview.jl")
 include("thermodynamics.jl")
 include("precompile.jl")
 
